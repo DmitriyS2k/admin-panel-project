79bd79f38a1c65607bf949a12ea0c7186e484676
DmitriyS2k <dimon16295@gmail.com>
2022-05-07 13:59:20 +0300

    edit axios cfg

--- src/API/ConfigAxios.ts
+++ src/API/ConfigAxios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
   timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' },
 });
 
 export default instance;
