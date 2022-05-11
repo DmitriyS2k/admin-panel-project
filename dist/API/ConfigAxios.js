3a9ef5989c6181191f895dea6e887b29c8fa4734
Dmitriy <93384288+DmitriyS2k@users.noreply.github.com>
2022-05-11 20:40:48 +0300

    Delete dist directory

--- dist/API/ConfigAxios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export var instance = axios.create({
-    baseURL: 'http://localhost:3000/',
-    timeout: 1000,
-    headers: { 'X-Custom-Header': 'foobar' },
-});
-export default instance;
-//# sourceMappingURL=ConfigAxios.js.map
--- dist/components/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import cl from '../styles/loader.module.css';
-function Loading() {
-    return React.createElement("div", { className: cl.loader });
-}
-export default Loading;
-//# sourceMappingURL=Loading.js.map
