0075540ea7475fe43ca91b13766339d5ec35da58
Dmitriy <93384288+DmitriyS2k@users.noreply.github.com>
2022-05-11 20:40:16 +0300

    Delete dist directory

--- dist/view/Error.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-function Error() {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "\u041E\u0439! \u041F\u043E\u0445\u043E\u0436\u0435, \u044D\u0442\u043E\u0439 \u0441\u0442\u0440\u0430\u043D\u0438\u0446\u044B \u043D\u0435\u0442!")));
-}
-export default Error;
-//# sourceMappingURL=Error.js.map
